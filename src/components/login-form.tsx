f4c901d6ff8b92570aec4eee207d0e095a1175ea
Rence Salaveria <cbsalaveria@ncmh.gov.ph>
2024-10-24 14:56:41 +0800

    feat (Dashboard/Login) Has now dashboard and log in

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,6 +9,8 @@ import {
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
 import {FaLink} from "react-icons/fa6";
+import {Formik} from "formik";
+import toast from "react-hot-toast";
 
 export function LoginForm() {
   return (
@@ -20,26 +22,34 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="text">Username</Label>
-            <Input
-              id="text"
-              type="text"
-              placeholder="Enter your username"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
+        <Formik initialValues={{
+          username: "",
+          password: "",
+        }} onSubmit={async (values) => toast(`${values.password}, ${values.username}`)}>
+          <form>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="text">Username</Label>
+                <Input
+                  id="text"
+                  type="text"
+                  placeholder="Enter your username"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required placeholder="Enter your password"/>
+              </div>
+              <Button type="button" className="w-full bg-primary">
+                Login
+              </Button>
             </div>
-            <Input id="password" type="password" required placeholder="Enter your password"/>
-          </div>
-          <Button type="submit" className="w-full bg-primary">
-            Login
-          </Button>
-        </div>
+          </form>
+
+        </Formik>
       </CardContent>
     </Card>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import AppRouter from "@/AppRouter.tsx";
+import {Toaster} from "react-hot-toast";
 
 const queryClient = new QueryClient()
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <div className="font-body bg-gradient-to-b from-primary to-white h-screen">
     <QueryClientProvider client={queryClient}>
       <StrictMode>
+        <Toaster/>
         <AppRouter/>
       </StrictMode>
     </QueryClientProvider>
