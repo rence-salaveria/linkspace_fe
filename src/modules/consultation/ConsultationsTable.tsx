77c873af17af5b38a817368f8b5d1d2976d28628
Rence Salaveria <cbsalaveria@ncmh.gov.ph>
2024-11-14 11:00:46 +0800

    feat (Add Student) Can now add student

--- src/modules/consultation/ConsultationsTable.tsx
+++ src/modules/consultation/ConsultationsTable.tsx
@@ -1,18 +1,18 @@
 import Navbar from "@/components/Navbar.tsx";
-import { useQuery } from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 import axios from "@/lib/axios.ts";
-import { Consultation } from "@/types/consultation.ts";
-import { useState } from "react";
-import { User } from "@/types/user.ts";
-import { ReusableTableComponent } from "@/components/reusable-table.tsx";
-import { createColumnHelper } from "@tanstack/react-table";
+import {Consultation} from "@/types/consultation.ts";
+import {useState} from "react";
+import {User} from "@/types/user.ts";
+import {ReusableTableComponent} from "@/components/reusable-table.tsx";
+import {createColumnHelper} from "@tanstack/react-table";
 import {FaRegPenToSquare} from "react-icons/fa6";
 
 const ConsultationsTable = () => {
   const [user] = useState<User>(JSON.parse(localStorage.getItem("user") || "{}"));
   const columnHelper = createColumnHelper<Consultation>();
 
-  const { data, isFetching } = useQuery<Consultation[]>({
+  const {data, isFetching} = useQuery<Consultation[]>({
     queryKey: ['consultation', user],
     queryFn: async () => {
       const res = await axios.get(`/consultation/${user.id}`);
@@ -68,8 +68,10 @@ const ConsultationsTable = () => {
       cell: (row) => {
         const value = row.getValue();
         return (
-          <span className={`relative inline-block px-3 py-1 font-semibold leading-tight rounded-full ${value === 'LookUp-001' ? 'bg-gray-500 text-white' : 'bg-primary text-white'}`}>
-            <span aria-hidden className={`absolute inset-0 ${value === 'LookUp-001' ? 'bg-gray-500' : 'bg-primary'} opacity-50 rounded-full`}></span>
+          <span
+            className={`relative inline-block px-3 py-1 font-semibold leading-tight rounded-full ${value === 'LookUp-001' ? 'bg-gray-500 text-white' : 'bg-primary text-white'}`}>
+            <span aria-hidden
+                  className={`absolute inset-0 ${value === 'LookUp-001' ? 'bg-gray-500' : 'bg-primary'} opacity-50 rounded-full`}></span>
             <span className="relative">{value === 'LookUp-001' ? 'Draft' : 'Scheduled'}</span>
           </span>
         );
@@ -99,11 +101,11 @@ const ConsultationsTable = () => {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <div className="container py-6">
-        <Navbar />
+        <Navbar/>
         <main className="flex-1 w-full flex justify-center">
           {isFetching ? <div>Loading...</div> : (
             // @ts-ignore
-            <ReusableTableComponent data={data ? data : []} columns={columns} tableTitle="Consultations" />
+            <ReusableTableComponent data={data ? data : []} columns={columns} tableTitle="Consultations"/>
           )}
         </main>
       </div>
